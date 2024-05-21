5a096d8b353ad5c340eeae7d45c400cd3bea9e1f
luggi85 <luggi.raider@gmail.com>
2024-05-21 23:16:03 +0200

    commit

--- Leistungsanalyse.py
+++ Leistungsanalyse.py
@@ -1,23 +1,25 @@
-import numpy as np
+import streamlit as st
 import pandas as pd
 import plotly.graph_objs as go
 
-
+# Daten laden
 activity_data = pd.read_csv('data/activity.csv')
 
-mittelwert_leistung = round(activity_data['PowerOriginal'].mean(),3)
-maximalwert_leistung = round(activity_data['PowerOriginal'].max(),3)
+mittelwert_leistung = round(activity_data['PowerOriginal'].mean(), 3)
+maximalwert_leistung = round(activity_data['PowerOriginal'].max(), 3)
 
-print(f"Mittelwert der Leistung: {mittelwert_leistung}")
-print(f"Maximalwert der Leistung: {maximalwert_leistung}")
-
-
-def plot_leistung_herzfrequenz(activity_data):
+def plot_leistung_herzfrequenz(activity_data, max_heartrate):
     # Plot erstellen
+    zonen_grenzen = heart_zones(max_heartrate)
     activity_data['CumulativeTime'] = activity_data['Duration'].cumsum()
     fig = go.Figure()
     fig.add_trace(go.Scatter(x=activity_data['CumulativeTime'], y=activity_data['PowerOriginal'], mode='lines', name='Leistung'))
     fig.add_trace(go.Scatter(x=activity_data['CumulativeTime'], y=activity_data['HeartRate'], mode='lines', name='Herzfrequenz', yaxis='y2'))
+    fig.add_hrect(y0=0, y1=zonen_grenzen[0], fillcolor='green', opacity=0.7, layer='below', line_width=0)
+    fig.add_hrect(y0=zonen_grenzen[0], y1=zonen_grenzen[1], fillcolor='yellow', opacity=0.7, layer='below', line_width=0)
+    fig.add_hrect(y0=zonen_grenzen[1], y1=zonen_grenzen[2], fillcolor='orange', opacity=0.7, layer='below', line_width=0)
+    fig.add_hrect(y0=zonen_grenzen[2], y1=zonen_grenzen[3], fillcolor='red', opacity=0.7, layer='below', line_width=0)
+    fig.add_hrect(y0=zonen_grenzen[3], y1=400, fillcolor='purple', opacity=0.7, layer='below', line_width=0)
 
     # Layout anpassen
     fig.update_layout(
@@ -34,13 +36,18 @@ def plot_leistung_herzfrequenz(activity_data):
             matches='y',
             range=[activity_data['HeartRate'].min(), activity_data['HeartRate'].max()]
         ),
-        legend=dict(orientation="h")
+        legend=dict(
+            orientation="v",
+            x=1,
+            y=1,
+            xanchor="right",
+            yanchor="top"
+        )
     )
 
     return fig
 
 def heart_zones(max_heartrate):
-
     # Herzfrequenzzonen definieren
     zonen_grenzen = [
         0.6 * max_heartrate,
@@ -63,15 +70,6 @@ def analyze_heart_zones(max_heartrate):
     zeit_in_zonen = activity_data['Zone'].value_counts()
 
     return zeit_in_zonen
-
-
-
-
-"""# Zonen zuordnen
-activity_data['Zone'] = pd.cut(activity_data['HeartRate'], bins=[0] + zonen_grenzen, labels=["Z1", "Z2", "Z3", "Z4", "Z5"], right=False)
-
-# Zeit in jeder Zone berechnen
-zeit_in_zonen = activity_data['Zone'].value_counts()"""
 
 if __name__ == "__main__":
     plot_leistung_herzfrequenz(activity_data).show()
--- main.py
+++ main.py
@@ -5,35 +5,39 @@ from PIL import Image
 import Leistungsanalyse as la
 import traceback
 
-
+# Setze das Seitenlayout auf "wide"
 st.set_page_config(layout="wide")
+
+# CSS-Stile definieren
 st.markdown("""
 <style>
 .big-font {
-    font-size:40px !important;
+    font-size: 40px !important;
     font-weight: bold;
 }
 .custom-font {
-    font-size:50px !important;
+    font-size: 50px !important;
     font-weight: bold;
+    color: darkred;
 }
 .custom-position1 {
     margin-top: 20px;
     margin-left: -50px;
 }
 .custom-position2 {
-    margin-top: -310px;
+    margin-top: -40px;
     margin-left: 200px;
+    position: absolute;
 }
+
 </style>
 """, unsafe_allow_html=True)
 
+# Callback-Funktion für Radiobutton-Auswahl
 def callback_function():
-    # Logging Message
     print(f"The user has changed to {st.session_state.current_user}")
 
-
-# Funktion zum Aktualisieren der Toggle-Buttons basierend auf den Änderungen
+# Toggle-Buttons aktualisieren
 def update_toggles(toggled_key):
     if toggled_key == 'toggle_1' and st.session_state.toggle_1:
         st.session_state.toggle_2 = False
@@ -41,108 +45,117 @@ def update_toggles(toggled_key):
     elif toggled_key == 'toggle_2' and st.session_state.toggle_2:
         st.session_state.toggle_1 = False
         st.session_state.diagram = 2
+    else:
+        st.session_state.diagram = None
 
+# Diagramm anzeigen
 def show_diagram():
+    if 'max_heartrate' not in st.session_state:
+        st.session_state['max_heartrate'] = 200
+
+    max_heartrate = st.session_state['max_heartrate']
+
     if st.session_state.get('diagram') == 1:
-        # Leistungsanalyse
-        st.plotly_chart(la.plot_leistung_herzfrequenz(la.activity_data))
+        if max_heartrate is not None:
+            st.plotly_chart(la.plot_leistung_herzfrequenz(la.activity_data, max_heartrate))
     elif st.session_state.get('diagram') == 2:
         st.write('Kein Diagramm ausgewählt')
 
+# Daten anzeigen
 def show_data():
     if st.session_state.get('diagram') == 1:
-        # Leistungsanalyse
-        max_heartrate = st.slider('Maximale Herzfrequenz', min_value=100, max_value=220, step=1)
-        st.write(f"Mittelwert der Leistung: {la.mittelwert_leistung} Watt")
-        st.write(f"Maximalwert der Leistung: {la.maximalwert_leistung} Watt")
+        max_heartrate = st.slider('Maximale Herzfrequenz', min_value=100, max_value=220, step=1, value=st.session_state.get('max_heartrate', 200))
+        st.session_state['max_heartrate'] = max_heartrate
+
+        # Spalten für die Kennzahlen
+        col1, col2 = st.columns(2)
+        with col1:
+            st.metric(label="Mittelwert der Leistung", value=f"{la.mittelwert_leistung} Watt")
+        with col2:
+            st.metric(label="Maximalwert der Leistung", value=f"{la.maximalwert_leistung} Watt")
+
         zeit_in_zonen = la.analyze_heart_zones(max_heartrate)
-        st.write(f"Zeit in den Zonen: {zeit_in_zonen} Sekunden")
-        
-        return max_heartrate
-   
 
-    #elif st.session_state.get('diagram') == 2:
-        # EKG-Analyse
-        #st.plotly_chart(la.plot_ekg(la.activity_data))  # Annahme: es gibt eine entsprechende Funktion
+        # Weitere Spalten für die Zeit in Zonen
+        col1, col2 = st.columns(2)
+        with col1:
+            st.metric(label="Zeit in Zone 1", value=f"{zeit_in_zonen['Z1']} s")
+            st.metric(label="Zeit in Zone 2", value=f"{zeit_in_zonen['Z2']} s")
+            st.metric(label="Zeit in Zone 3", value=f"{zeit_in_zonen['Z3']} s")
+        with col2:
+            st.metric(label="Zeit in Zone 4", value=f"{zeit_in_zonen['Z4']} s")
+            st.metric(label="Zeit in Zone 5", value=f"{zeit_in_zonen['Z5']} s")
 
- #Initialisiere die Toggle-Buttons im Session State
+# Initialisiere die Toggle-Buttons im Session State
 if 'toggle_1' not in st.session_state:
     st.session_state.toggle_1 = False
 if 'toggle_2' not in st.session_state:
     st.session_state.toggle_2 = False
-
-with st.sidebar:
-    col1, col2 = st.columns(2)
-    with col1:
-        #App Logo
-        st.image('/Users/lukas/Documents/MCI/Software_engineering/HerzAktiv/Logo2.jpeg', width=100)
-    with col2:
-        # Eine Überschrift der ersten Ebene
-        st.markdown('<p class="custom-position1 big-font">HerzAktiv</p>', unsafe_allow_html=True)
-    st.write('Webseite zur Visualisierung der Leistungs- und EKG-Daten.')
-    # Eine Überschrift der zweiten Ebene
-    st.subheader("Versuchsperson auswählen:")
-
-    # Session State wird leer angelegt, solange er noch nicht existiert
-    if 'current_user' not in st.session_state:
-        st.session_state.current_user = 'None'
-
-    # Legen Sie eine neue Liste mit den Personennamen an indem Sie ihre Funktionen aufrufen
-    person_dict = rd.load_person_data()
-    person_names = rd.get_person_list(person_dict)
-
-    st.session_state.current_user = st.radio(
-        'Versuchsperson',
-        options = person_names, key="sbVersuchsperson", on_change = callback_function)
-    
-    #st.write('<style>div.row-widget.stRadio > div{flex-direction:row;justify-content: center;} </style>', unsafe_allow_html=True)
-
-    #st.write('<style>div.st-bf{flex-direction:column;} div.st-ag{font-weight:bold;padding-left:2px;}</style>', unsafe_allow_html=True)
-    
-    # Checkbox als Toggle-Button für die erste Option
-    Leistungs_button = st.toggle('Leistungs-Analyse', value=st.session_state.toggle_1, key='toggle_1', on_change=update_toggles, args=('toggle_1',))
-
-    # Checkbox als Toggle-Button für die zweite Option
-    EKG_button = st.toggle('EKG-Analyse', value=st.session_state.toggle_2, key='toggle_2', on_change=update_toggles, args=('toggle_2',))
-
-            
-
-
-col1, col2 = st.columns([1,2]) 
-# Eine Überschrift der ersten Ebene
-if st.session_state.get('diagram') == 1:
-    st.markdown('<p class="custom-position2 custom-font">Leistungsdaten</p>', unsafe_allow_html=True)
-elif st.session_state.get('diagram') == 2:
-    st.markdown('<p class="custom-position2 custom-font">EKG-Daten</p>', unsafe_allow_html=True)
-elif st.session_state.get('diagram') == None:
-    st.markdown('<p class="custom-position2 custom-font">HerzAktiv <3</p>', unsafe_allow_html=True)
-
-with col1:
-    # Anlegen des Session State. Bild, wenn es kein Bild gibt
-    if 'picture_path' not in st.session_state:
-        st.session_state.picture_path = 'data/pictures/none.jpg'
-
-    # Suche den Pfad zum Bild, aber nur wenn der Name bekannt ist
-    if st.session_state.current_user in person_names:
-        st.session_state.picture_path = rd.find_person_data_by_name(st.session_state.current_user)['picture_path']
-
-    # Öffne das Bild und Zeige es an
-    image = Image.open("/Users/lukas/Documents/MCI/Software_engineering/HerzAktiv/" + st.session_state.picture_path)
-    st.image(image, caption=st.session_state.current_user + '(' + str(rd.find_age(st.session_state.current_user)) + ')')
-
-with col2:
-    show_data()
-
-
 if 'diagram' not in st.session_state:
     st.session_state.diagram = None
 
-# Rufe show_diagram am Ende des Skripts auf
+# Seitenleiste
+with st.sidebar:
+    # Logo und Titel
+    col1, col2 = st.columns(2)
+    with col1:
+        st.image('/Users/lukas/Documents/MCI/Software_engineering/HerzAktiv/Logo2.jpeg', width=100)
+    with col2:
+        st.markdown('<p class="custom-position1 big-font">HerzAktiv</p>', unsafe_allow_html=True)
+    st.write('Webseite zur Visualisierung der Leistungs- und EKG-Daten.')
+
+    # Toggle-Buttons
+    Leistungs_button = st.toggle('Leistungs-Analyse', value=st.session_state.toggle_1, key='toggle_1', on_change=update_toggles, args=('toggle_1',))
+    EKG_button = st.toggle('EKG-Analyse', value=st.session_state.toggle_2, key='toggle_2', on_change=update_toggles, args=('toggle_2',))
+
+    # Radiobuttons für Versuchsperson-Auswahl bei aktivierter EKG-Analyse
+    if st.session_state.toggle_2:
+        if 'current_user' not in st.session_state:
+            st.session_state.current_user = 'None'
+
+        person_dict = rd.load_person_data()
+        person_names = rd.get_person_list(person_dict)
+
+        st.session_state.current_user = st.radio(
+            'Versuchsperson',
+            options=person_names, key="sbVersuchsperson", on_change=callback_function
+        )
+
+# Überschrift und Datenanzeige basierend auf der Auswahl
+header_html = ""
+if st.session_state.get('diagram') == 1:
+    header_html = '<p class="custom-position2 custom-font">Leistungsdaten</p>'
+    st.markdown(header_html, unsafe_allow_html=True)
+    show_data()
+elif st.session_state.get('diagram') == 2:
+    header_html = '<p class="custom-position2 custom-font">EKG-Daten</p>'
+    st.markdown(header_html, unsafe_allow_html=True)
+elif st.session_state.get('diagram') is None:
+    header_html = '<p class="custom-position2 custom-font">HerzAktiv <3</p>'
+    st.markdown(header_html, unsafe_allow_html=True)
+    col1, col2, col3, col4 = st.columns(4)
+    with col2:
+        image = Image.open("/Users/lukas/Documents/MCI/Software_engineering/HerzAktiv/data/pictures/sila_lukas.png")
+        st.image(image, caption="Erstellt von Sila und Lukas", width=350)
+
+# Bild und Datenanzeige bei ausgewählter EKG-Analyse
+if st.session_state.get('diagram') == 2:
+    col1, col2 = st.columns([1, 2])
+    with col1:
+        if 'picture_path' not in st.session_state:
+            st.session_state.picture_path = 'data/pictures/none.jpg'
+
+        if st.session_state.current_user in person_names:
+            st.session_state.picture_path = rd.find_person_data_by_name(st.session_state.current_user)['picture_path']
+        image = Image.open("/Users/lukas/Documents/MCI/Software_engineering/HerzAktiv/" + st.session_state.picture_path)
+        st.image(image, caption=f"{st.session_state.current_user} ({rd.find_age(st.session_state.current_user)})")
+    with col2:
+        st.write('Keine Daten vorhanden')
+
+# Diagramm am Ende des Skripts aufrufen
 if 'diagram' in st.session_state:
     try:
         show_diagram()
-        # Dein Code, der das Diagramm erzeugt
-        #st.plotly_chart(la.plot_leistung_herzfrequenz(la.activity_data), theme = 'streamlit', use_container_width=True)
     except Exception as e:
         st.error(f"Ein Fehler ist aufgetreten: {e}")
         st.text('Fehler-Traceback:')
